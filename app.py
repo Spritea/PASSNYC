7a3f77309908fd2db7f232a69bcfe94267c9a8ba
Spritea <461158649@qq.com>
2021-04-21 01:08:24 +0800

    add PCA BiPlot

--- app.py
+++ app.py
@@ -9,13 +9,61 @@ from sklearn.manifold import MDS
 from sklearn.metrics import pairwise_distances
 app = Flask(__name__)
 
+class My_PCA:
+    def __init__(self,csv_file,label_col):
+        self.data_out=self.change_percent(self.remove_cat(csv_file))
+        self.original_df,self.pca_components=self.compute_PCA(self.data_out)
+        self.label,self.label_unique=self.get_data_label(csv_file,label_col)
+
+    def get_data_label(self,csv_file,label_col):
+        df_in = pd.read_csv(csv_file)
+        # cat_list = ["City", "Rigorous Instruction Rating", "Collaborative Teachers Rating",
+        #             "Supportive Environment Rating", "Effective School Leadership Rating",
+        #             "Strong Family-Community Ties Rating","Trust Rating","Student Achievement Rating"]
+        label=list(df_in[label_col])
+        label_unique = list(np.unique(df_in[label_col]))
+        return label,label_unique
+    def remove_cat(self,csv_file):
+        df_in = pd.read_csv(csv_file)
+        cat_list = ["City", "Rigorous Instruction Rating", "Collaborative Teachers Rating",
+                    "Supportive Environment Rating", "Effective School Leadership Rating",
+                    "Strong Family-Community Ties Rating","Trust Rating","Student Achievement Rating"]
+        df_out = df_in.drop(cat_list, axis=1)
+        return df_out
+
+    #transform percent to decimal.
+    def change_percent(self,csv_data):
+        percent_list = ["Percent ELL", "Student Attendance Rate", "Percent of Students Chronically Absent",
+                    "Rigorous Instruction %", "Collaborative Teachers %","Supportive Environment %",
+                    "Effective School Leadership %","Strong Family-Community Ties %","Trust %",]
+        trans_func=lambda col: col.str.strip('%').astype(float)/100
+        csv_data[percent_list]=csv_data[percent_list].apply(trans_func,axis=1)
+        return csv_data
+
+    def compute_PCA(self,df):
+        StandScaler = StandardScaler()
+        #no set n_components, it would save all components.
+        pca = PCA(random_state=0)
+        original_df = pca.fit_transform(StandScaler.fit_transform(df))
+        # pca_percent = 100 * pca.explained_variance_ratio_
+        # pca_cum_sum_var = np.cumsum(pca_percent)
+        return original_df,pca.components_
+
+
+pca_class=My_PCA(csv_file='school_data_clean.csv',label_col="City")
+print('kk')
 
 @app.route('/')
 def index():
     # return 'Hello World!'
     return render_template("index.html")
 
-
+@app.route("/plotTop2PCA", methods=['POST', 'GET'])
+def plotTop2OriginalPCA():
+    if request.method == 'POST':
+        points_data = list(zip(pca_class.original_df[:, 0], pca_class.original_df[:, 1],pca_class.label))
+        axis_data = list(zip(pca_class.pca_components[0, :], pca_class.pca_components[1, :]))
+        return json.dumps([points_data, axis_data,pca_class.label_unique])
 
 if __name__ == '__main__':
     app.run()
---
+++ static/js/BiPlot.js
@@ -0,0 +1,178 @@
+function biPlot() {
+    // d3.select("svg").remove();
+    var functionName = "/plotTop2PCA";
+    // $("#div_table").empty();
+    // $("div.tooltip").remove();
+
+    $.post(functionName, function (data) {
+        var dataAll = JSON.parse(data);
+        var dataArray = dataAll[0];
+        var axisArray = dataAll[1];
+        var label_unique=dataAll[2];
+        // var data_color_list = ["#00FFFF", "#37BFE5", "#8A2BE2", "#0000FF", "#FF1493", "#00FF00", "#FFA500"];
+
+        var color_list = ['aqua', 'blue', 'fuchsia', '#5F9EA0', 'green', 'lime', 'maroon', 'navy', '#008B8B',
+            'purple', 'red', '#A52A2A', 'teal', 'yellow'];
+        var attr_names = ['ENI', 'SIE', 'PELL', 'SAR', 'PSCA',
+            'RI', 'CT', 'SE', 'ESL', 'SFCT', 'TS', 'AELA', 'AMP', 'AP'];
+
+        var margin = {top: 20 * 2, right: 170, bottom: 70, left: 50},
+            width = 640 - margin.left - margin.right,
+            height = 420 - margin.top - margin.bottom;
+
+        // var svg=d3.select("#biplotsvg")
+
+        var svg = d3.select("#biplotsvg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .attr("id", "canvas")
+            // .attr("transform", "translate(" + 300 + "," + 70 + ")")
+            .append("g")
+            .attr("transform", "translate(" + (margin.left + 50) + "," + (margin.top + 10) + ")");
+
+        var x = d3.scaleLinear()
+            .range([0, width])
+            .domain([d3.min(dataArray, function (d) {
+                return d[0];
+            }), d3.max(dataArray, function (d) {
+                return d[0];
+            })]);
+        var y = d3.scaleLinear()
+            .range([height, 0])
+            //to accommodate x.
+            .domain([d3.min(dataArray, function (d) {
+                return d[1];
+            }), d3.max(dataArray, function (d) {
+                return d[1];
+            })]);
+
+        var xAxis = d3.axisBottom(x);
+
+        var yAxis = d3.axisLeft(y)
+        // .ticks(10);
+
+        svg.append('g')
+            .selectAll("dot")
+            .data(dataArray)
+            .enter()
+            .append("circle")
+            .attr("cx", function (d) {
+                return x(d[0]);
+            })
+            .attr("cy", function (d) {
+                return y(d[1]);
+            })
+            .attr("r", 3)
+            .style("fill", function (d) {
+                return color_list[label_unique.indexOf(d[2])];
+            });
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+
+        d3.selectAll('.x.axis')
+            .selectAll("text")
+            .style('font-size', 'medium');
+        d3.selectAll('.y.axis')
+            .selectAll("text")
+            .style('font-size', 'medium');
+
+        svg.append("text")
+            .attr("transform",
+                "translate(" + (width / 2) + " ," +
+                (height + margin.top + 20) + ")")
+            .style("text-anchor", "middle")
+            .text("PC1")
+            .style("font-weight", "bold");
+
+        svg.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 - margin.left - 2 - 10)
+            .attr("x", 0 - (height / 2))
+            .attr("dy", "1em")
+            .style("text-anchor", "middle")
+            .text("PC2")
+            .style("font-weight", "bold");
+
+        svg.append('g')
+            .selectAll("marker")
+            .data(attr_names)
+            .enter()
+            .append("marker")
+            // .attr("refX", 6)
+            // .attr("refY", 6)
+            .attr("id", function (d, i) {
+                return "triangle" + i;
+            })
+            .attr("refX", 3)
+            .attr("refY", 3)
+            .attr("markerWidth", 30)
+            .attr("markerHeight", 30)
+            .attr("orient", "auto")
+            .append("path")
+            // .attr("d", "M 0 0 12 6 0 12 3 6")
+            .attr("d", "M 0 0 6 3 0 6 1.5 3")
+            .attr('fill', function (d, i) {
+                return color_list[i];
+            });
+
+        svg.append('g')
+            .selectAll("axis_arrow")
+            .data(axisArray)
+            .enter()
+            .append("line")
+            .attr("x1", function (d) {
+                return x(0);
+            })
+            .attr("y1", function (d) {
+                return y(0);
+            })
+            .attr("x2", function (d) {
+                return x(9 * d[0]);
+            })
+            .attr("y2", function (d) {
+                return y(9 * d[1]);
+            })
+            .attr("stroke-width", 1.7)
+            .attr("stroke", function (d, i) {
+                return color_list[i];
+            })
+            .attr("marker-end", function (d, i) {
+                return "url(#triangle" + i + ")";
+            });
+
+        var legend = svg.selectAll(".legend")
+            .data(attr_names)
+            .enter().append("g")
+            .attr("class", "legend")
+            .attr("transform", function (d, i) {
+                return `translate(0, ${(i * 20 + 20)})`;
+            });
+
+        legend.append("rect")
+            .attr("x", innerWidth - 50)
+            .attr("y", 8)
+            .attr("width", 40)
+            .attr("height", 3)
+            .style("fill", function (d, i) {
+                return color_list[i];
+            });
+
+        legend.append("text")
+            .attr("x", innerWidth - 75)
+            .attr("y", 15)
+            .style("text-anchor", "end")
+            .style("font-weight", "bold")
+            .style("font-size", "10pt")
+            .text(function (d, i) {
+                return attr_names[i];
+            });
+    });
+
+}
